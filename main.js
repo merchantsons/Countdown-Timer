dc46ed6bf64f3ffa7aca528c15ef6af2660ee829
merchantsons <160190585+merchantsons@users.noreply.github.com>
2024-05-18 06:08:12 -0700

    Final Commit

---
+++ main.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Enter Valid Seconds Where You Wan't Countdown Starts From?",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please Enter Valid Number!";
+        }
+        else if (input > 60) {
+            return "Number Should Be Up To 60 Seconds!";
+        }
+        else {
+            return true;
+        }
+    }
+});
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+startTime(input);
